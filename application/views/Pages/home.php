416c9862cebe834795e182563e804b0e5d61883d
Rahul Mitra <rahul@infimonk.com>
2020-09-21 17:39:58 +0530

    changes

--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -1,7 +1,7 @@
 <?= View("Pages/header"); ?>
     <!-- body part -->
     <div class="productIframe">
-      <iframe src="https://japan.cheersagar.com/assets/japanhtm/index.htm#media=2" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://japan.cheersagar.com/assets/WebPublish/index.htm" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
      
    </body>
